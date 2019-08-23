3337bf5dfd4a7e238b20c2bfa258db7c2b5cb6ca
Jose Guadalupe Omar Ramirez Mata <Jose.Ramirez@municipiodequeretaro.gob.mx>
2019-08-23 16:54:10 -0500

    Version 2-V28

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 // componente de clase
 import React from 'react'
 import circlesImg from '../images/circles.png'
+import emptyImg from '../images/empty.png'
 import './styles/Card.css'
 
 
@@ -26,12 +27,12 @@ class Card extends React.Component {
         return (
             <div className="card mx-auto Fitness-Card"
                 style={{
-                    backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})`
+                    backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor||'#a8c0ff'}, ${rightColor||'#3f2b96'})`
                 }}>
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img className="float-right" src={img} alt="exercise"/>
+                            <img className="float-right" src={img || emptyImg} alt="exercise"/>
                         </div>
                         <div className="col-6 Fitness-Card-Info">
                             <h1>{title}</h1>
--- src/pages/ExerciseNew.js
+++ src/pages/ExerciseNew.js
@@ -1,16 +1,87 @@
 import React from 'react'
+import ExerciseForm from '../components/ExerciseForm'
+import Card from '../components/Card'
+import FatalError from './500'
 
 class ExerciseNew extends React.Component {
 
-    handleClick = () => {
-        console.log(this)
+    state = {
+        form: {
+            title: '',
+            description: '',
+            img: '',
+            leftColor: '',
+            rightColor: ''
+        },
+        loading: false,
+        error: null
     }
 
+    handleChange = e => {
+        this.setState({
+            form: {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    handleSubmit = async e => {
+        this.state({
+            loading: true
+        })
+
+        e.preventDefault()
+        try {
+            console.log('Try')
+            let config = {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(this.state.form)
+            }
+
+            let res = await fetch('http://localhost:8000/api/exercises', config)
+            let json = await res.json()
+
+            this.state({
+                loading: false
+            })
+
+            this.props.history.push('/exercise')
+
+        } catch (error) {
+            console.log('Error')
+            this.state({
+                loading: false,
+                error
+            })
+        }
+    }
+    
     render() {
+
+        if (this.state.error)
+            return <FatalError/>
+
         return (
-            <button onClick={this.handleClick}>
-                Send
-            </button>
+            <div className="row">
+                <div className="col-sm">
+                    <Card 
+                        {...this.state.form}
+                    />
+                </div>
+                <div className="col-sm">
+                    <ExerciseForm
+                        onChange = {this.handleChange}
+                        onSubmit = {this.handleSubmit}
+                        form = {this.state.form}
+                    />
+                </div>
+            </div>
+            
         )
     }
 }
